ca9e854b06677e65eecd2dbb5135b9239ccafb09
Fabian Fabro <fabrofabian@gmail.com>
2019-02-20 04:30:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -34,5 +34,5 @@ function incrementVariable() {
 function doWhileLoop(num){
   do{
       console.log("I run once regardless.")
-  }while(incrementVariable() < num);
+  }while(incrementVariable() <= num);
 }

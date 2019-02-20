2c1f98a41d78078f523994da6090f689a28ec4eb
Fabian Fabro <fabrofabian@gmail.com>
2019-02-20 01:27:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,12 +4,12 @@ function forLoop(array){
     for(let i = 0; i < 25; i++){
       if (i === 1){
         newString = "I am ${i} strange loop.";
-        array.push(newString);
+        array.push(i);
         //console.log("I am " + i + " strange loop.");
       }
       else{
         newString = "I am ${i} strange loops${i === 0 ? '' : 's'}.";
-        array.push(newString);
+        array.push(i);
         //console.log("I am ${i} strange loops.");
       }
       return array;

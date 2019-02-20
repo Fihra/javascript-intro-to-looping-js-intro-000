396485873e88cd5d40b853ea2370331023b03181
Fabian Fabro <fabrofabian@gmail.com>
2019-02-20 04:59:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,17 +2,15 @@ function forLoop(array){
   //var newString;
     for(let i = 0; i < 25; i++){
       if (i === 1){
-        //console.log(`"I am ${i} strange loop."`);
-        //array.push(i);
-        array[i] = "I am ${i} strange loop.";
-        //console.log("I am " + i + " strange loop.");
+        array[i].push("I am ${i} strange loop.");
+        //array[i] = "I am ${i} strange loop.";
       }
       else{
         //newString = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
        //array.push(newString);
+        array[i].push("I am ${i} strange loops.");
         array[i] = "I am ${i} strange loops.";
-        //array[i] = "I am ${i} strange loop${i === 0 ? '' : 's'}.";
-        //console.log("I am ${i} strange loops.");
+        array[i].push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
       }
       return array;
     }

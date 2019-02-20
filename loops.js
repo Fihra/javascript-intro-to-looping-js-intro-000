681709ec1ee734571c0651acb3ab4eecf26be9b8
Fabian Fabro <fabrofabian@gmail.com>
2019-02-20 05:11:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
     for(let i = 0; i < 25; i++){
       if (i === 1){
-        array.push(`"I am ${i} strange loop."`);
+        array.push(`I am ${i} strange loop.`);
       }
       else{
-        array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+        array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
       }
     }return array;
 }
